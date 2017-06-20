edf945615a3b6bc744f37375da87b6bf4c666b7d
Fernando Tong <nandorov@gmail.com>
2017-06-20 00:29:03 -0500

    adding questions

--- 06-JS/sandbox.js
+++ 06-JS/sandbox.js
@@ -12,3 +12,19 @@ while (i < 4 ) {
   console.log('hola'); 
 }
 
+
+
+Preguntas Alumnos
+
+Arreglos
+
+Cómo se agregar un elemento
+Cómo se lee un elemento
+Cómo se elimina un elemento
+Cómo se actualiza un elemento
+
+¿Para qué sirve la estructura FOR?
+Cuál es la declaración
+Cuál es la condición
+Cuál es la asignación
+
